802ae7c3f9f78be420e4d35a360711d51863aa43
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-12-15 19:45:30 -0500

    Bash script that display the distributor ID in a concise single-line output

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsb_release -i | cut -d: -f2 | tr -d '[:space:]'
+lsb_release -i -s
--- cybersecurity_basics/0x00_introduction_cybersecurity/compare.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-decoded=$(echo "$1" | base64 --decode 2>/dev/null) && [[ "$decoded" == *"lsb_release"* ]] && echo ok || echo invalid
