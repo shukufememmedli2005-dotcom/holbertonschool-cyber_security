af627f4af96f22e550a85accce809cf57ea55a14
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2026-01-18 13:13:14 -0500

    s

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,6 @@
-!/bin/bash
-curl -X POST $2 -H "Host: $1" -d $3
+#!/bin/bash
+
+curl -X POST "$2" \
+     -H "Host: $1" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     --data "$3"
